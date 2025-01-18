81bbcbc062c42cde116ca6b2bf6e2312d0e0471f
EdwardKerckhof <edwardkerckhof@hotmail.com>
2025-01-18 16:28:51 +0100

    feat: cleanup RKE terraform

--- RKE2/main.tf
+++ RKE2/main.tf
@@ -1,13 +1,7 @@
-variable "masters_count" {
-  description = "Amount of master nodes to create"
+variable "nodes_count" {
+  description = "Amount of nodes to create"
   type        = number
-  default     = 1
-}
-
-variable "workers_count" {
-  description = "Amount of worker nodes to create"
-  type        = number
-  default     = 2
+  default     = 5
 }
 
 variable "vm_template" {
@@ -16,29 +10,29 @@ variable "vm_template" {
   default     = "ubuntu-cloud"
 }
 
-resource "proxmox_vm_qemu" "masters" {
-  count = var.masters_count
-  name = format("master-%02d", count.index + 1)
+resource "proxmox_vm_qemu" "nodes" {
+  count       = var.nodes_count
+  name        = format("rke-node-%02d", count.index + 1)
   target_node = "hydra"
-  vmid = 300 + count.index
-  desc = format("Master node %02d", count.index + 1)
-  tags = "kubernetes,rke2,ubuntu"
+  vmid        = 300 + count.index
+  desc        = format("RKE2 node %02d", count.index + 1)
+  tags        = "kubernetes,rke2,ubuntu"
 
-  clone = var.vm_template
+  clone      = var.vm_template
   full_clone = true
 
-  agent = 1
-  cores = 2
+  agent   = 1
+  cores   = 2
   sockets = 1
-  cpu = "host"
-  memory = 2048
+  cpu     = "host"
+  memory  = 4096
   os_type = "cloud-init"
 
   scsihw   = "virtio-scsi-single"
   bootdisk = "scsi0"
 
   vga {
-    type = "std"
+    type   = "std"
     memory = 4
   }
 
@@ -52,7 +46,7 @@ resource "proxmox_vm_qemu" "masters" {
       scsi0 {
         disk {
           storage = "apps"
-          size = 20
+          size    = 20
         }
       }
     }
@@ -66,65 +60,7 @@ resource "proxmox_vm_qemu" "masters" {
   }
 
   ipconfig0 = format("ip=10.8.8.%d/24,gw=10.8.8.1", 160 + count.index)
-  ciuser = "edward"
-  ciupgrade = true
-
-  sshkeys = <<EOF
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGMnM797XOLqn/sc6mx9PyxWwed+neKWG+E2AdxJfguH edwardkerckhof@hotmail.com
-  EOF
-}
-
-resource "proxmox_vm_qemu" "workers" {
-  count = var.workers_count
-  name = format("worker-%02d", count.index + 1)
-  target_node = "hydra"
-  vmid = 310 + count.index
-  desc = format("Worker node %02d", count.index + 1)
-  tags = "kubernetes,rke2,ubuntu"
-
-  clone = var.vm_template
-  full_clone = true
-
-  agent = 1
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 2048
-  os_type = "cloud-init"
-
-  scsihw   = "virtio-scsi-single"
-  bootdisk = "scsi0"
-
-  vga {
-    type = "std"
-    memory = 4
-  }
-
-  network {
-    bridge = "vmbr0"
-    model  = "virtio"
-  }
-
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          storage = "apps"
-          size = 20
-        }
-      }
-    }
-    ide {
-      ide2 {
-        cloudinit {
-          storage = "apps"
-        }
-      }
-    }
-  }
-
-  ipconfig0 = format("ip=10.8.8.%d/24,gw=10.8.8.1", 165 + count.index)
-  ciuser = "edward"
+  ciuser    = "edward"
   ciupgrade = true
 
   sshkeys = <<EOF
